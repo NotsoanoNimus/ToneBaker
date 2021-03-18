24b532320261665c9946934f9c2fc880073e8381
Notsoano Nimus <github@xmit.xyz>
2021-03-17 21:17:33 -0400

    MP3 Boilerplate
    Boilerplate code for MP3 streaming. Getting ready to dig into the algorithm and file format.

---
+++ ToneBaker/MP3/Mp3Stream.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using ToneBaker.PCM;
+
+namespace ToneBaker.MP3 {
+    public sealed class Mp3FileFormat {
+
+    }
+
+
+    public sealed class Mp3Stream : IDisposable {
+        internal bool _disposed = false;
+        public Mp3Stream(ref List<PCMSample> rawAudioStream) { }
+
+        public void Dispose() {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        internal void Dispose(bool disposing) {
+            if(!this._disposed) {
+                if(disposing) { /* set members null */ }
+                // Indicate the instance has been disposed.
+                this._disposed = true;
+            }
+        }
+    }
+}
--- ToneBakerTesting/Program.cs
+++ ToneBakerTesting/Program.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using ToneBaker.PCM;
 using ToneBaker.WAV;
+using ToneBaker.MP3;
 
 namespace ToneBakerTesting {
     class Program {
